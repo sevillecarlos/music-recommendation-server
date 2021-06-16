f2457db2ccbc87d6fa84a72d13d2e79f18cadf5a
sevillecarlos <sevillecarlos@gmail.com>
2021-06-16 01:13:20 -0600

    add the save-tracks api

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 require("dotenv").config();
-
+//routes////////////////////////////////
 const spotifyAuth = require("./routes/spotify-auth");
 const auth = require("./routes/auth");
-
+const musicRecommendation = require("./routes/music-recommendation");
+const saveTrack = require("./routes/save-track");
+////////////////////////////////////////
 const app = express();
 const port = process.env.PORT || 3000;
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.73fv4.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
 app.use(cors());
-app.use(express.json()).use(auth).use(spotifyAuth);
+app
+  .use(express.json())
+  .use(auth)
+  .use(spotifyAuth)
+  .use(musicRecommendation)
+  .use(saveTrack);
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
---
+++ routes/music-recommendation.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const request = require("request");
+
+router.post("/recommendation", (req, res) => {
+  request.get(
+    {
+      url:
+        "https://api.spotify.com/v1/recommendations?seed_artists=" +
+        req.body.seedArtist +
+        "&seed_tracks=" +
+        req.body.seedTrack,
+      headers: {
+        Authorization: "Bearer " + req.body.accessToken,
+      },
+
+      json: true,
+    },
+    function (error, response, body) {
+      res.send(body);
+    }
+  );
+});
+module.exports = router;

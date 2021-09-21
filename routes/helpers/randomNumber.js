faca7215eb8b58bbd98921f46685aec80deb477c
Carlos Sevilla <sevillecarlos@gmail.com>
2021-09-21 15:10:39 -0600

    add helpers

--- routes/helpers/randomNumber.js
+++
@@ -1,2 +0,0 @@
-module.exports = randomNumber = (max, min) =>
-  Math.floor(Math.random() * max - min) + min;
--- routes/helpers/removeParathensisText.js
+++
@@ -1,2 +0,0 @@
-module.exports = removeParanthesisContent = (text) =>
-  text.indexOf("(") !== -1 ? text.slice(0, text.indexOf("(")) : text;
